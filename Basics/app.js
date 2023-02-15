a52f62cd6d4382496fe1a722b61b12b24ee10099
Sugu2023 <123944031+Sugu2023@users.noreply.github.com>
2023-02-15 21:59:13 +0530

    Create app.js

---
+++ Basics/app.js
@@ -0,0 +1,20 @@
+//--About variables..
+let myAge;
+myAge=38;
+console.log(myAge);
+var my_age;
+my_age=56;
+console.log(typeof(my_age));
+const myage=20;
+console.log(myage);
+
+//Array
+let newArray=[10,50,30];
+console.log(newArray[1]);
+console.log(typeof(newArray));
+
+//Numbers
+const myInt=24;
+const myFloat=2.4;
+console.log(typeof(myInt));
+console.log(typeof(myFloat));
