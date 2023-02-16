5d03ce7f13d8efae15eb15d73f19ef57a9d3f620
Sugu2023 <123944031+Sugu2023@users.noreply.github.com>
2023-02-16 13:51:25 +0530

    Update String.js

--- Basics/String.js
+++ Basics/String.js
@@ -48,3 +48,73 @@ const output=`Virat smashed more runs in the odi series scoring ${total} runs in
 His average of this series is ${(total/4)}`;
 console.log(output);
 
+//String Methods
+
+const demoName='Sugumaran';
+const len=demoName.length;
+console.log(len);
+console.log(demoName[6]);//using this we can get that particular letter
+
+//substring
+ const joined='SuguSuve';
+ if(joined.startsWith('Sugu'))
+ {
+    console.log('Sugu is found');
+ }
+ else
+ {
+    console.log('No sugu is found');
+ }
+ if(joined.endsWith('Suve'))
+ {
+    console.log('Suve is found');
+ }
+ else
+ {
+    console.log('No Suve is found');
+ }
+//Position of a String
+
+const sentence='HTML-It is a language used to develop website.It is easy to understand';
+console.log(sentence.indexOf('used'));//prints the starting index of the word.
+
+let firstOccur=sentence.indexOf('is');
+let secOccur=sentence.indexOf('is',firstOccur+1);
+console.log(firstOccur);
+console.log(secOccur);
+
+//Substring in a String
+
+const wordPlat='Thirumurugan';
+console.log(wordPlat.slice(0,5));
+console.log(wordPlat.slice(5));
+
+//Case
+
+const lowerCase='sugu';
+const upperCase=lowerCase.toUpperCase();
+console.log(upperCase);
+let UpperCase='SUVE';
+const newLow=UpperCase.toLocaleLowerCase();
+console.log(newLow);
+
+let demo='mozila';
+demo=demo.replace('mo','god');
+console.log(demo);
+let newsent='He is a mental fellow, He can do coding all the day!';
+newsent=newsent.replaceAll('He','She');
+console.log(newsent);
+
+//
+const list=document.querySelector('ul');
+list.innerHTML='';
+const greetings=['Rishi is play boy','Selva is rugges boy','Rishi is a content creator'];
+for(const dret of greetings)
+{
+    if(dret.includes('Rishi'))
+    {
+        const listItem=document.createElement('li');
+        listItem.textContent=dret;
+        list.appendChild(listItem);
+    }
+}
